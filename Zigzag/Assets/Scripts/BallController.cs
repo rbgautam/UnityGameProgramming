ac9b0ca9cc5c6be1f2c7fd02ede2947ae6ca9e26
deltaman2000 <rahulbgautam@gmail.com>
2017-12-20 21:02:25 -0600

    Added scripts and game behaviour

--- Zigzag/Assets/Scripts/BallController.cs
+++ Zigzag/Assets/Scripts/BallController.cs
@@ -2,15 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallController : MonoBehaviour {
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    bool started;
+    bool gameover;
+    Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Use this for initialization
+    void Start()
+    {
+        started = false;
+        gameover = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!started)
+        {
+
+            if (Input.GetMouseButton(0))
+            {
+                rb.velocity = new Vector3(speed, 0, 0);
+                started = true;
+            }
+        }
+
+        if (!Physics.Raycast(transform.position, Vector3.down, 1f))
+        {
+            gameover = true;
+            rb.velocity= new Vector3(0, -20f, 0);
+        }
+
+
+
+        if (Input.GetMouseButton(0) && !gameover)
+        {
+            SwitchDirection();
+        }
+    }
+
+
+    void SwitchDirection()
+    {
+
+        if (rb.velocity.z > 0)
+        {
+            rb.velocity = new Vector3(speed, 0, 0);
+        }
+        else if (rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector3(0, 0, speed);
+
+        }
+    }
 }
